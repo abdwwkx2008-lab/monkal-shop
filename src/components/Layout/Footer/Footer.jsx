b327320cdb1c6bd6941583946cf8301223994a65
abdwwkx2008-lab <abdwwkx2008@gmail.com>
2025-08-25 15:40:48 +0600

    feat: eplace footer logo with new brand logo

--- src/components/Layout/Footer/Footer.jsx
+++ src/components/Layout/Footer/Footer.jsx
@@ -6,7 +6,7 @@ import { CustomContext } from '../../../store/store.jsx';
 import Tg from '/assets/tg.icon.svg';
 import WhatsApp from '/assets/whatsApp.icon.svg';
 import Inst from '/assets/inst.icon2.png';
-import Footerlogo from '/assets/footer-logo.png';
+import Footerlogo from '/assets/header-monkal-logo-removebg-preview.png';
 
 function Footer() {
     const { user, logOutUser } = useContext(CustomContext);
